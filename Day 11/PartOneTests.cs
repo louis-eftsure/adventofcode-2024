08ec6bf518d2ca3e8de2feab616fa924c3be52a5
Louis van Schalwkyk <louiss@eftsure.com.au>
2024-12-10 22:57:20 +1100

    Adding Base Files D10

---
+++ Day 11/PartOneTests.cs
@@ -0,0 +1,24 @@
+using Shouldly;
+
+namespace Day_11;
+
+public class PartOneTests
+{
+    [Fact]
+    public async Task ExampleInput()
+    {
+        var input = await File.ReadAllLinesAsync("Inputs/example-input.txt");
+        var result = input.Length;
+        
+        result.ShouldBe(0);
+    }
+    
+    [Fact]
+    public async Task PuzzleInput()
+    {
+        var input = await File.ReadAllLinesAsync("Inputs/input.txt");
+        var result = input.Length;
+        
+        result.ShouldBe(0);
+    }
+}
